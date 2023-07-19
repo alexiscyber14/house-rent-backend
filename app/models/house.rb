50403be45fa9e3947d96fc80e3f21305acb80cd5
Thiri <thirihtet246@gmail.com>
2023-07-19 22:18:52 +0630

    Add house_spec

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,4 @@
 class House < ApplicationRecord
+    validates :name, presence: true
+    validates :rental_fee, presence: true
 end
---
+++ spec/models/house_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe House, type: :model do
+  before(:each) do
+    @house = House.create(name:'one-floor',area: 100,rental_fee: 1000)
+  end
+
+  describe 'validations' do
+    it 'should be vaild with valid attributes' do
+      expect(@house).to be_valid
+    end
+
+    it 'should not be vaild without name' do
+        @house.name = nil
+        expect(@house).not_to be_valid
+      end
+
+      it 'should not be vaild without rental fee' do
+        @house.rental_fee = nil
+        expect(@house).not_to be_valid
+      end
+  end
+end
