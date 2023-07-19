33ffc06dcdb92382be3f8c0c6b202abb52069add
Thiri <thirihtet246@gmail.com>
2023-07-19 21:37:21 +0630

    Add reservation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  has_many :reservations
 end
