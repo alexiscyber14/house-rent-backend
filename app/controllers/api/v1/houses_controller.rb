06533d2b96343edf8b13f4fab20e75f753a7f18d
alexiscyber14 <142sledgehammer14@gmail.com>
2023-07-20 17:19:17 +0100

    add rubocp linter fix

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,74 +1,73 @@
 # app/controllers/api/v1/houses_controller.rb
 module Api
-    module V1
-      class HousesController < ApplicationController
-        before_action :authenticate_user!, only: [:create, :destroy]
-        before_action :set_house, only: [:show, :edit, :update, :destroy]
-        respond_to :json
-  
-        # GET /api/v1/houses
-        def index
-          @houses = House.all
-          render json: @houses
+  module V1
+    class HousesController < ApplicationController
+      before_action :authenticate_user!, only: %i[create destroy]
+      before_action :set_house, only: %i[show edit update destroy]
+      respond_to :json
+
+      # GET /api/v1/houses
+      def index
+        @houses = House.all
+        render json: @houses
+      end
+
+      # GET /api/v1/houses/1
+      def show
+        render json: @house
+      end
+
+      # POST /api/v1/houses
+      def create
+        @house = current_user.houses.build(house_params)
+
+        if @house.save
+          render json: @house, status: :created
+        else
+          render json: @house.errors, status: :unprocessable_entity
         end
-  
-        # GET /api/v1/houses/1
-        def show
+      end
+
+      # PATCH/PUT /api/v1/houses/1
+      def update
+        unless current_user == @house.user
+          render_json_response('You are not authorized to update this house.', :unauthorized)
+          return
+        end
+
+        if @house.update(house_params)
           render json: @house
+        else
+          render json: @house.errors, status: :unprocessable_entity
         end
-  
-        # POST /api/v1/houses
-        def create
-          @house = current_user.houses.build(house_params)
-  
-          if @house.save
-            render json: @house, status: :created
-          else
-            render json: @house.errors, status: :unprocessable_entity
-          end
+      end
+
+      # DELETE /api/v1/houses/1
+      def destroy
+        unless current_user == @house.user
+          render_json_response('You are not authorized to delete this house.', :unauthorized)
+          return
         end
-  
-        # PATCH/PUT /api/v1/houses/1
-        def update
-          unless current_user == @house.user
-            render_json_response("You are not authorized to update this house.", :unauthorized)
-            return
-          end
-  
-          if @house.update(house_params)
-            render json: @house
-          else
-            render json: @house.errors, status: :unprocessable_entity
-          end
-        end
-  
-        # DELETE /api/v1/houses/1
-        def destroy
-          unless current_user == @house.user
-            render_json_response("You are not authorized to delete this house.", :unauthorized)
-            return
-          end
-  
-          @house.destroy
-          render_json_response("House was successfully deleted.")
-        end
-  
-        private
-  
-        def set_house
-          @house = House.find(params[:id])
-        rescue ActiveRecord::RecordNotFound
-          render_json_response("House not found.", :not_found)
-        end
-  
-        def house_params
-          params.require(:house).permit(:name, :location, :rental_fee, :date_built, :category, :description, :image_url)
-        end
-  
-        def render_json_response(message, status=:ok)
-          render json: { message: message }, status: status
-        end
+
+        @house.destroy
+        render_json_response('House was successfully deleted.')
+      end
+
+      private
+
+      def set_house
+        @house = House.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render_json_response('House not found.', :not_found)
+      end
+
+      def house_params
+        params.require(:house).permit(:name, :location, :rental_fee, :date_built, :category, :description, :image_url)
+      end
+
+      def render_json_response(message, status = :ok)
+        render json: { message: }, status:
       end
     end
   end
-  
+end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,78 +1,76 @@
 class HousesController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
-    before_action :set_house, only: [:show, :edit, :update, :destroy]
-    respond_to :json
-    
-    # GET /houses
-    def index
-      @houses = House.all
-    end
-  
-    # GET /houses/1
-    def show
-    end
-  
-    # GET /houses/new
-    def new
-      @house = current_user.houses.build
-    end
-  
-    # GET /houses/1/edit
-    def edit
-      # Ensure the current user owns the house before allowing editing
-      unless current_user == @house.user
-        redirect_to @house, alert: "You are not authorized to edit this house."
-      end
-    end
-  
-    # POST /houses
-    def create
-      @house = current_user.houses.build(house_params)
-  
-      if @house.save
-        redirect_to @house, notice: "House was successfully created."
-      else
-        render :new
-      end
-    end
-  
-    # PATCH/PUT /houses/1
-    def update
-      # Ensure the current user owns the house before allowing updating
-      unless current_user == @house.user
-        redirect_to @house, alert: "You are not authorized to update this house."
-        return
-      end
-  
-      if @house.update(house_params)
-        redirect_to @house, notice: "House was successfully updated."
-      else
-        render :edit
-      end
-    end
-  
-    # DELETE /houses/1
-    def destroy
-      # Ensure the current user owns the house before allowing deletion
-      unless current_user == @house.user
-        redirect_to @house, alert: "You are not authorized to delete this house."
-        return
-      end
-  
-      @house.destroy
-      redirect_to houses_url, notice: "House was successfully deleted."
-    end
-  
-    private
-  
-    # Use callbacks to share common setup or constraints between actions.
-    def set_house
-      @house = House.find(params[:id])
-    end
-  
-    # Only allow a list of trusted parameters through.
-    def house_params
-      params.require(:house).permit(:name, :rental_fee, :date_built, :category, :description, :image_url)
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :set_house, only: %i[show edit update destroy]
+  respond_to :json
+
+  # GET /houses
+  def index
+    @houses = House.all
+  end
+
+  # GET /houses/1
+  def show; end
+
+  # GET /houses/new
+  def new
+    @house = current_user.houses.build
+  end
+
+  # GET /houses/1/edit
+  def edit
+    # Ensure the current user owns the house before allowing editing
+    return if current_user == @house.user
+
+    redirect_to @house, alert: 'You are not authorized to edit this house.'
+  end
+
+  # POST /houses
+  def create
+    @house = current_user.houses.build(house_params)
+
+    if @house.save
+      redirect_to @house, notice: 'House was successfully created.'
+    else
+      render :new
     end
   end
-  
+
+  # PATCH/PUT /houses/1
+  def update
+    # Ensure the current user owns the house before allowing updating
+    unless current_user == @house.user
+      redirect_to @house, alert: 'You are not authorized to update this house.'
+      return
+    end
+
+    if @house.update(house_params)
+      redirect_to @house, notice: 'House was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /houses/1
+  def destroy
+    # Ensure the current user owns the house before allowing deletion
+    unless current_user == @house.user
+      redirect_to @house, alert: 'You are not authorized to delete this house.'
+      return
+    end
+
+    @house.destroy
+    redirect_to houses_url, notice: 'House was successfully deleted.'
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_house
+    @house = House.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def house_params
+    params.require(:house).permit(:name, :rental_fee, :date_built, :category, :description, :image_url)
+  end
+end
