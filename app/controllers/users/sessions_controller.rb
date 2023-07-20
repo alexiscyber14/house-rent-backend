83d3a6f30bfa1c31c4ac78a2397d709f520a7dba
Thiri <thirihtet246@gmail.com>
2023-07-20 11:35:40 +0630

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,21 +7,24 @@ class Users::SessionsController < Devise::SessionsController
   def respond_with(_resource, _opts = {})
     render json: {
       message: 'Logged in successfully.',
-      user: current_user
+      data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
     }, status: :ok
   end
 
-  def on_destroy
-    success_logged_out && return if current_user
-
-    fail_logged_out
-  end
-
-  def success_logged_out
-    render json: { message: 'logged out sucessfully' }, status: :ok
-  end
-
-  def fail_logged_out
-    render json: { message: 'Logged out failed' }, status: :unauthorized
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
+    
+    if current_user
+      render json: {
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
   end
 end
