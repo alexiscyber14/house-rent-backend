f453ab45b3f9e4ef49d0d0e4db14a87ca326c204
Thiri <thirihtet246@gmail.com>
2023-07-21 20:43:31 +0630

    Update key for sign out

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,19 +1,20 @@
 class MembersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  def index
+    user = User.all
+    if current_user.role == 'admin'
+      render json: user
+    else
+      render json: { message: 'You are not authorized to view this page.' }
+    end
+  end
+
   def show
-    user = user_from_token
+    user = current_user
     render json: {
       message: 'success',
       user:
     }
-  end
-
-  private
-
-  def user_from_token
-    jwt_payload = JWT.decode(request.headers['Authorization'].split[1],
-                             Rails.application.credentials.devise[:jwt_secret_key]).first
-    user_id = jwt_payload['sub']
-    User.find(user_id.to_s)
   end
 end
