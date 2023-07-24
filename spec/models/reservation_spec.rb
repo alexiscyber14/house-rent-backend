d0877618f77e290720e802bc13dd4a45e03bf5f1
alexiscyber14 <142sledgehammer14@gmail.com>
2023-07-24 08:05:35 +0100

    adjust tests

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -3,45 +3,58 @@ require 'devise'
 
 RSpec.describe Reservation, type: :model do
   before(:each) do
-    user = User.create(email: 'user@example.com', password: 'password')
-    house = House.create(name: 'one-floor', area: 100, rental_fee: 1000)
-    @reservation = Reservation.create(user:, house:, location: 'yangon',
-                                      reservation_date: '2021-07-19 15:04:28')
+    user = User.create(name: "Alex", email: 'user@example.com', password: 'password', password_confirmation: "password")
+    @house = House.create(
+      name: 'one-floor',
+      location: '100',
+      rental_fee: 1000,
+      date_built: '2000-01-01',
+      category: 'apartment',
+      description: 'very nice',
+      image_url: 'https://goodle.com/image.pn',
+      user: user # Set the user association here
+    )
+    @reservation = Reservation.create(
+      user: user,
+      house: @house,
+      city: 'yangon',
+      reservation_date: '2021-07-19 15:04:28'
+    )
   end
 
   describe 'validations' do
-    it 'should be vaild with valid attributes' do
+    it 'should be valid with valid attributes' do
       expect(@reservation).to be_valid
     end
 
-    it 'should not be vaild without user' do
+    it 'should not be valid without user' do
       @reservation.user = nil
       expect(@reservation).not_to be_valid
     end
 
-    it 'should not be vaild without house' do
+    it 'should not be valid without house' do
       @reservation.house = nil
       expect(@reservation).not_to be_valid
     end
 
-    it 'should not be vaild without location' do
-      @reservation.location = nil
+    it 'should not be valid without location' do
+      @reservation.city = nil
       expect(@reservation).not_to be_valid
     end
 
-    it 'should not be vaild without reservation date' do
+    it 'should not be valid without reservation date' do
       @reservation.reservation_date = nil
       expect(@reservation).not_to be_valid
     end
   end
 
   describe 'associations' do
-    it 'should have belongs to house' do
+    it 'should belong to house' do
       house_reservation = Reservation.reflect_on_association(:house)
       expect(house_reservation.macro).to eq(:belongs_to)
     end
 
-    it 'should have belongs to user' do
+    it 'should belong to user' do
       user_reservation = Reservation.reflect_on_association(:user)
       expect(user_reservation.macro).to eq(:belongs_to)
     end
