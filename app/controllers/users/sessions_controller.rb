4f0a22c48391dfd415f668785e87af3400c7c7b7
Thiri <thirihtet246@gmail.com>
2023-07-27 14:20:14 +0630

    Update session and authorization

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,21 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
   include ActionController::MimeResponds
   include RackSessionsFix
-
   respond_to :json
-
-  def create
-    user = current_user
-    respond_with user
-  end
-
-  def destroy
-    sign_out(current_user)
-
-    render json: {
-      message: 'Logged out successfully.'
-    }, status: :ok
-  end
 
   private
 
@@ -25,5 +11,32 @@ class Users::SessionsController < Devise::SessionsController
       message: 'Logged in successfully.',
       data: { user: }
     }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
+                               Rails.application.config.jwt_secret_key,
+                               true, { algorithm: 'HS256' }).first
+      current_user = User.find(jwt_payload['sub'])
+    end
+
+    if current_user
+      success_signout
+    else
+      fail_signout
+    end
+  end
+
+  def success_signout
+    render json: {
+      message: 'Logged out successfully.'
+    }, status: :ok
+  end
+
+  def fail_signout
+    render json: {
+      message: "Couldn't find an active session."
+    }, status: :unauthorized
   end
 end
